4822dd1c3e660a1bfcd1a3a5865877e648df7f50
Stephen Ball <stephen@rebelinblue.com>
2016-02-08 20:04:37 +0000

    Added URL argument

--- src/Commands/ScraperCommand.php
+++ src/Commands/ScraperCommand.php
@@ -3,6 +3,7 @@
 namespace REBELinBLUE\Sainsburys\Commands;
 
 use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
@@ -12,6 +13,11 @@ use Symfony\Component\Console\Output\OutputInterface;
  */
 class ScraperCommand extends Command
 {
+    /**
+     * Configures the command instance.
+     *
+     * @return void
+     */
     protected function configure()
     {
         $this->setName('scrape')
@@ -22,10 +28,26 @@ class ScraperCommand extends Command
                  InputOption::VALUE_NONE,         // Mode
                  'Pretty Prints the JSON output', // Description,
                  null                             // Default
+             )
+             ->addArgument(
+                'url',                            // Option
+                InputArgument::REQUIRED,          // Mode
+                'URL to scrape'                   // Description
              );
     }
 
+    /**
+     * Executes the command.
+     *
+     * @param  InputInterface  $input
+     * @param  OutputInterface $output
+     * @return void
+     */
     protected function execute(InputInterface $input, OutputInterface $output)
     {
+        $pretty = $input->getOption('pretty');
+        $url = $input->getArgument('url');
+
+        $scraper = new Scraper;
     }
 }
