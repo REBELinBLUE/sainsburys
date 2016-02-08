6c982064c668d490ef28a206a241de3535ce8eb1
Stephen Ball <stephen@rebelinblue.com>
2016-02-08 22:22:01 +0000

    Adding PHPDoc blocks

--- src/Crawler/ProductParser.php
+++ src/Crawler/ProductParser.php
@@ -6,27 +6,61 @@ use REBELinBLUE\Sainsburys\Models\Product;
 use Symfony\Component\DomCrawler\Crawler;
 use REBELinBLUE\Sainsburys\Scraper;
 
+/**
+ * A class to parse the product block.
+ */
 class ProductParser
 {
+    /**
+     * The Crawler oject containing the product block.
+     * @var Crawler
+     */
     private $crawler;
+
+    /**
+     * The Crawler object for the product details page.
+     * @var Crawler
+     */
     private $details;
+
+    /**
+     * The content length of the product details page.
+     * @var integer
+     */
     private $length;
 
+    /**
+     * Class constructor
+     *
+     * @param Crawler $crawler The product block from the DOM
+     */
     public function __construct(Crawler $crawler)
     {
         $this->crawler = $crawler;
     }
 
+    /**
+     * Gets the product model for the Crawler passed to the constructor.
+     *
+     * @return Product
+     */
     public function getProduct()
     {
-        return [
-            'title' => $this->getTitle(),
-            'size' => $this->getPageSize(),
-            'description' => $this->getDescription(),
-            'unit_price' => $this->getUnitPrice()
-        ];
+        $product = new Product;
+
+        $product->title = $this->getTitle();
+        $product->size = $this->getPageSize();
+        $product->description = $this->getDescription();
+        $product->unit_price = $this->getUnitPrice();
+
+        return $product;
     }
 
+    /**
+     * Extracts the title from the Crawler.
+     *
+     * @return string
+     */
     private function getTitle()
     {
         $title = $this->crawler
@@ -36,7 +70,13 @@ class ProductParser
         return trim($title);
     }
 
-    private function getUnitPrice() // FIXME: Convert to float
+    /**
+     * Extracts the unit price from the Crawler.
+     *
+     * @return string
+     * @todo Convert to number
+     */
+    private function getUnitPrice()
     {
         $price = $this->crawler
                       ->filter('.addToTrolleytabBox .pricePerUnit')
@@ -45,6 +85,11 @@ class ProductParser
         return str_replace(['&pound', '/unit'], '', trim($price));
     }
 
+    /**
+     * Gets the details page.
+     *
+     * @return Crawler
+     */
     private function getDetails()
     {
         if (is_null($this->details))
@@ -62,13 +107,23 @@ class ProductParser
         return $this->details;
     }
 
+    /**
+     * Gets the description from the details page.
+     *
+     * @return string
+     */
     private function getDescription()
     {
         $details = $this->getDetails();
 
-        return '';
+        return trim($details->filter('.productText')->text());
     }
 
+    /**
+     * Gets the size of the details page.
+     *
+     * @return string
+     */
     private function getPageSize()
     {
         $this->getDetails();
