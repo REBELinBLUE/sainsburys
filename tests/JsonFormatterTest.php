6d214fe56db34e947ab9055d76fc74429249a799
Stephen Ball <stephen@nudgecrm.com>
2016-02-09 18:06:18 +0000

    Testing the JSON data formatting

--- tests/JsonFormatterTest.php
+++ tests/JsonFormatterTest.php
@@ -8,10 +8,43 @@ use REBELinBLUE\Sainsburys\Formatters\JsonFormatter;
 class JsonFormatterTest extends \PHPUnit_Framework_TestCase
 {
     /**
-     * Skip the tests
-     **/
-    public function testSkip()
+     * Tests that an empty array returns the expected object.
+     */
+    public function testEmpty()
     {
-        $this->markTestSkipped('Not yet implemented');
+        $formatter = new JsonFormatter([]);
+
+        $this->assertEquals('[]', $formatter->getFormatted());
+
+        // Make sure that turning on pretty doesn't change the output
+        $formatter->setPretty(true);
+        $this->assertEquals('[]', $formatter->getFormatted());
+
+        // Then that turning off pretty doesn't break it
+        $formatter->setPretty(false);
+        $this->assertEquals('[]', $formatter->getFormatted());
+    }
+
+    /**
+     * Test the formatter outputs a simple key/value correctly
+     **/
+    public function testSingleEntry()
+    {
+        $simple_json = file_get_contents(__DIR__ . '/fixtures/simple.json');
+        $formatted_json = file_get_contents(__DIR__ . '/fixtures/formatted_simple.json');
+
+        $formatter = new JsonFormatter([
+            'key' => 'value'
+        ]);
+
+        $this->assertEquals($simple_json, $formatter->getFormatted());
+
+        // Make sure that turning on pretty doesn't change the output
+        $formatter->setPretty(true);
+        $this->assertEquals($formatted_json, $formatter->getFormatted());
+
+        // Then that turning off pretty means the unformatted data is returned
+        $formatter->setPretty(false);
+        $this->assertEquals($simple_json, $formatter->getFormatted());
     }
 }
