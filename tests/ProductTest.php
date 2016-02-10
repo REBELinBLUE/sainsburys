8a87415e31af983e7c822d3cb34b6208dd25a5f5
Stephen Ball <stephen@nudgecrm.com>
2016-02-10 14:21:50 +0000

    Test product model

--- tests/ProductTest.php
+++ tests/ProductTest.php
@@ -10,10 +10,21 @@ use REBELinBLUE\Sainsburys\Models\Product;
 class ProductTest extends \PHPUnit_Framework_TestCase
 {
     /**
-     * Skip the tests
+     * Test that the product class is allowing properties to be used as an array
      **/
-    public function testSkip()
+    public function testArrayProperties()
     {
-        $this->markTestSkipped('Not yet implemented');
+        $product = new Product;
+        $product->title = 'Peaches';
+
+        $this->assertInstanceOf('\ArrayAccess', $product);
+
+        $this->assertEquals('Peaches', $product['title']);
+
+        $productAsJson = json_encode($product);
+        $this->assertEquals('{"title":"Peaches"}', $productAsJson);
+
+        $productAsArray = json_decode($productAsJson, true);
+        $this->assertArrayHasKey('title', $productAsArray);
     }
 }
