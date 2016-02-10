6140ae3e27c1a23e69d1cb8ff098dc29f4c75f4d
Stephen Ball <stephen@nudgecrm.com>
2016-02-10 13:20:12 +0000

    Test the Console Command class

--- tests/ScrapeCommandTest.php
+++ tests/ScrapeCommandTest.php
@@ -5,6 +5,7 @@ namespace REBELinBLUE\Sainsburys\Tests;
 use REBELinBLUE\Sainsburys\Commands\ScraperCommand;
 use Symfony\Component\Console\Application;
 use Symfony\Component\Console\Tester\CommandTester;
+use Symfony\Component\Console\Output\OutputInterface;
 
 /**
  * Test the Scrape Command
@@ -12,25 +13,58 @@ use Symfony\Component\Console\Tester\CommandTester;
 class ScrapeCommandTest extends \PHPUnit_Framework_TestCase
 {
     /**
-     * Text the basic application.
+     * Command Tester class
+     * @var CommandTester
+     */
+    private $commandTester;
+
+    /**
+     * Sets up the command tester.
      *
      * @return void
      */
-    public function testExecute()
+    public function setUp()
     {
-        $this->assertTrue(true);
-        return;
-
         $application = new Application;
         $application->add(new ScraperCommand);
 
         $command = $application->find('scrape');
 
-        $commandTester = new CommandTester($command);
-        $commandTester->execute([
-            'command' => $command->getName()
-        ]);
+        $this->commandTester = new CommandTester($command);
+    }
 
-        $this->assertEmpty($commandTester->getDisplay());
+    /**
+     * Test the verbosity setting works.
+     *
+     * @return void
+     */
+    public function testVerbosity()
+    {
+        $commandTester = $this->commandTester;
+
+        $commandTester->execute([
+            'command'   => 'scrape',
+            'url'       => 'http://www.google.com'
+        ], ['verbosity' => OutputInterface::VERBOSITY_VERBOSE]);
+
+        $this->assertRegExp('#Processing URL http://www.google.com#', $commandTester->getDisplay());
+    }
+
+    /**
+     * Test the verbosity setting works.
+     *
+     * @return void
+     */
+    public function testPrettyVerbosity()
+    {
+        $commandTester = $this->commandTester;
+
+        $commandTester->execute([
+            'command'   => 'scrape',
+            'url'       => 'http://www.google.com',
+            '--pretty'  => true,
+        ], ['verbosity' => OutputInterface::VERBOSITY_VERBOSE]);
+
+        $this->assertRegExp('/Enabled pretty printing/', $commandTester->getDisplay());
     }
 }
